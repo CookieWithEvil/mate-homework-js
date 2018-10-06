d72dff76870f1febda26bd38a97583d7bc81601a
Mita <mitafrumoasa@gmail.com>
2018-10-06 12:32:42 +0300

    upd. JS1

--- JS1/script.js
+++ JS1/script.js
@@ -1,3 +1,4 @@
+//PART 1
 //1
 var r = '9' + 0;//'90'
 console.log(r);
@@ -53,3 +54,65 @@ console.log(r);
 // console.log(r);
 // var r = (1++) - (2--);//-1
 // console.log(r);
+
+//PART 2
+//1
+var t = {
+    y: 9
+};
+var number = t.y;
+number += 5;
+console.log(t.y, number); // 9, 14
+//2
+var t = {
+    y: [1, 3, 5, 7]
+};
+var number = t.y[2];//5
+for(var i = 0, len = t.y.length; i < len; i++) {
+    t.y[i]++;//2, 4, 6, 8
+}
+console.log(number); // 5
+//3
+var t = 9;
+var obj = {
+    number: t
+};
+obj.number++;
+console.log(obj.number, t);//10, 9
+//4
+var object1 = {
+    str: {
+        name: "L"
+    }
+};
+
+var obj = {
+    number: 67
+};
+
+object1.str.number = obj.number;//67
+obj.number++;//68
+
+var str = object1.str;//str: {name: "L", number: 67}
+
+object1.str.name = "Kolya";
+
+console.log(str, object1.str.number);//str: {name: "Kolya", number: 67}, 67
+//5
+var object1 = {
+    obj: {
+        number: 0
+    }
+};
+
+var object2 = {
+    str: "Kolya"
+};
+
+object2.str.number = object2.str.number;//undefined
+
+object2.str.number++;//NaN
+
+console.log(object2.str.number);//undefined (i don't know why)
+
+console.log(++object2.str.number);//NaN
