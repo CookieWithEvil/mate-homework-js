781e5dad3a8a01fd484242d249192c686084bdeb
Mita <mitafrumoasa@gmail.com>
2018-10-19 11:22:29 +0300

    added js2

--- JS2/2/script.js
+++ JS2/2/script.js
@@ -8,6 +8,7 @@ function f1() {
 	}
 	document.body.appendChild(ul);
 }
+//2
 function f2() {
 	let ul = document.createElement('ul');
 	ul.className = `task2`;
@@ -25,33 +26,39 @@ function f2() {
 	}
 	document.body.appendChild(ul);
 }
+//3
 function f3(a, c) {
 	let div = document.createElement('div');
-	var primes = getPrimes(Math.min(a, c));
-	var gcd = 1;
+	// var primes = getPrimes(Math.min(a, c));	
 	div.className = `task3`;
-	div.innerHTML = `<h4>${div.className}</h4>`;		
-	for (var i = 0; i < primes.length; i++) {
-		if((a % primes[i] === 0) && (c % primes[i] === 0)){
-			gcd *= primes[i];
+	div.innerHTML = `<h4>${div.className}</h4>`;	
+	var gcd = 1;	
+	if(Math.max(a, c)%Math.min(a, c) === 0) gcd = Math.min(a, c);
+	else{
+		for (var i = Math.min(a, c); i > 1; i--) {
+			while(a % i == 0 && c % i == 0){
+				a /= i; c /= i;
+				gcd *= i;
+			}
 		}
-	}
+	}	
 	div.innerHTML += `GCD: ${gcd}`;
 	document.body.appendChild(div);
 }
-function getPrimes(num){//function returns all primes <= num
-	var primes = [2,];
-	for(var i = 1; i < num+1; i++){
-		for (var j = i-1; j >= 1; j--) {
-			if(i % j == 0){ 
-				break;
-			}else if((i % j != 0) && (j - 1 == 1)){
-				primes.push(i);
-			}
-		}
-	}
-	return primes;
-}
+// function getPrimes(num){//function returns all primes <= num
+// 	var primes = [2,];
+// 	for(var i = 1; i < num+1; i++){
+// 		for (var j = i-1; j >= 1; j--) {
+// 			if(i % j == 0){ 
+// 				break;
+// 			}else if((i % j != 0) && (j - 1 == 1)){
+// 				primes.push(i);
+// 			}
+// 		}
+// 	}
+// 	return primes;
+// }
+//4
 function f4() {
 	let ul = document.createElement('ul');
 	ul.className = `task4`;
@@ -63,18 +70,21 @@ function f4() {
 	ul.innerHTML += `Sum: ${sum}`;
 	document.body.appendChild(ul);
 }
+//5
 function f5() {
 	console.log("numbers that divide to 5 and 3 between 10-90:");
 	for (var i = 10; i < 91; i++) {
 		if((i%3==0) && (i%5==0)) console.log(i);
 	}
 }
+//6
 function f6() {
 	console.log("odd numbers between 70-11 :");
 	for (var i = 70; i > 10; i--) {
 		if(i % 2 !== 0) console.log(i);
 	}
 }
+//7
 function f7() {
 	console.log("even numbers that divide to 2 and 3 from range 78-13 :");
 	for (var i = 78; i > 12; i--) {
@@ -84,7 +94,7 @@ function f7() {
 
 f1();
 f2();
-// f3(25, 100);
+f3(24, 18);//6
 f4();
 f5();
 f6();
--- JS2/3/script.js
+++ JS2/3/script.js
@@ -1,95 +1,62 @@
-function f1() {
-	let ul = document.createElement('ul');
-	ul.className = `task1`;
-	ul.innerHTML = `<h4>${ul.className}</h4>`;
-	for (var i = 1; i < 51; i++) {
-		ul.innerHTML += `<li> ${i} </li>`;
-	}
-	document.body.appendChild(ul);
-}
-function f2() {
-	const arr = [1, 2, 3, 4, 5];
-	let ul = document.createElement('ul');
-	ul.className = `task2`;
-	ul.innerHTML = `<h4>${ul.className}</h4>`;
-	for (var i = 0; i < arr.length; i++) {
-		ul.innerHTML += `<li> ${arr[i]} </li>`;
-	}
-	document.body.appendChild(ul);
-}
-function f3() {
-	const arr = [2, 3, 4, 5];
-	let c = 1;//future composition
-	let div = document.createElement('div');
-	div.className = `task3`;
-	div.innerHTML = `<h4>${div.className}</h4>`;
-	div.innerHTML += `Composition: `;	
-	for (var i = 0; i < arr.length; i++) {
-		c *= arr[i];
-	}
-	div.innerHTML += `${c}`;
-	document.body.appendChild(div);
-}
-function f4() {
-	let obj = {'Минск':'Беларусь', 'Москва':'Россия', 'Киев':'Украина'};
-	let ul = document.createElement('ul');
-	ul.className = `task4`;
-	ul.innerHTML = `<h4>${ul.className}</h4>`;
-	for (key in obj) {
-		ul.innerHTML += `<li> ${key} - это ${obj[key]}</li>`;
-	}
-	document.body.appendChild(ul);
-}
-function f5() {
-	let num = 1000;
-	let i = 1;
-	let div = document.createElement('div');
-	div.className = `task5`;
-	div.innerHTML = `<h4>${div.className}</h4>`;
-	do{
-		num /= 2;
-		i++;
-	}while(num >= 50);
-	div.innerHTML += `The number will be: ${num} <br /> And iterations: ${i}`;
-	document.body.appendChild(div);
-}
-function f6() {
-	const arr = [2, 5, 9, 15, 0, 4];
-	let c = 1;//future composition
-	let div = document.createElement('div');
-	div.className = `task6`;	
-	div.innerHTML = `<h4>${div.className}</h4>`;
-	for (var i = 0; i < arr.length; i++) {
-		if(arr[i] > 3 && arr[i] < 10){
-			div.innerHTML += `${arr[i]}`;
-		}
-	}	
-	document.body.appendChild(div);
-}
-function f7() {
-	let ul = document.createElement('ul');
-	ul.className = `task7`;
-	ul.innerHTML = `<h4>${ul.className}</h4>`;
-	var date = new Date(),
-    locale = "en-us",
-    month = date.toLocaleString(locale, { month: "long" });
-    var m;
-	for (var i = 0; i < 12; i++) {
-		date.setMonth(i);
-		m = date.toLocaleString(locale, { month: "long" });
-		if(m === month){
-			ul.innerHTML += `<li><b>${m}</b></li>`;
-		}else{
-			ul.innerHTML += `<li>${m}</li>`;
-		}				
-	}
-	document.body.appendChild(ul);
+//1
+var one = "1" + 2 + 3;//"123"
+var two = 4 + false + "5" + 6;//"456"
+var three = 7 + true + 6 + "8";//"148"
+var four = false + true;//1(true)
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
+//2
+var t = 0;
+var h = 8;
+console.log(t && h && console.log(9));//0
+//3
+var pr = 9;
+var g = 0;
+console.log((g || pr) && console.log(9));//undefined
+//4
+var i = '1';
+var j = 8;
+var m = 7;
+
+(m - j) == j;//false
+(m - j) === j;//false
+
+console.log((m - 6) && j && console.log('Hi'));//undefined
+//5
+var obj = {
+    h: 0,
+    i: 'hello'
 }
 
-f1();
-f2();
-f3();
-f4();
-f5();
-f6();
-f7();
+var phrase = obj.i;//'hello'
+delete obj.i;
+
+console.log(phrase);//'hello'
+//6
+var obj = {
+    h: [1,2,3],
+    i: {
+     y: 9
+    }
+}
+
+var num = obj.i.y;//9
+
+obj.i.y = obj.i.y + 1;//10
+
+console.log(num, obj.i.y);//9, 10
+//7
+var obj = {
+    h: [1,2,3],
+    i: {
+     y: 9
+    }
+}
+
+var arr = obj.h;//[1,2,3]
+
+obj.h[1] = 9;//[1,9,3]
+
+console.log(arr[1], obj.h[1]);//9, 9
